752ae878a09865e3afc9f8fe7f605baa46ca10cc
SabaDodashvili <sabadodashvili777@gmail.com>
2021-09-27 04:06:26 +0400

    first draft

--- scripts.js
+++ scripts.js
@@ -22,3 +22,38 @@ function initialState(themeName) {
 
 localStorage.getItem('theme') !== 'null' ? initialState(localStorage.getItem('theme')) : initialState('light-theme');
 //=========================================== T H E M E  C H A N G E R ==================================================
+let actionsBtns = document.querySelector('.game-manu__actions');
+let startBtn = document.querySelector('.welcome__start-btn');
+let leftTime = document.querySelector('.game__title span');
+let screens = document.querySelectorAll('.full-screen');
+let time;
+
+startBtn.addEventListener('click', (e) => {
+  const currentScreen = e.target.closest('.full-screen');
+  currentScreen.classList.add('up');
+});
+
+actionsBtns.addEventListener('click', chooseTime);
+
+function chooseTime(e) {
+  const currentScreen = e.target.closest('.full-screen');
+  let seconds = parseInt(e.target.getAttribute('data-time'));
+  time = seconds;
+  currentScreen.classList.add('up');
+  setInterval(() => {
+    startGame();
+  }, 1000);
+}
+
+function startGame() {
+  if (time < 0) {
+    return;
+  } else {
+    if (time >= 10) {
+      leftTime.textContent = `00:${time}`;
+    } else {
+      leftTime.textContent = `00:0${time}`;
+    }
+    time--;
+  }
+}
